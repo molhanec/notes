05ea16c7fcc96a81a1ec632d365fb4d929093d37
Michal Molhanec <michal@molhanec.net>
2020-03-31 14:04:00 +0200

    Add ConnectedRouter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,19 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
+import { Action, configureStore, getDefaultMiddleware, ThunkAction } from '@reduxjs/toolkit'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
 
 import counterReducer from '../features/counter/counterSlice'
 import notesReducer from '../features/notes/notesSlice'
 
+export const history = createBrowserHistory()
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    router: connectRouter(history) as any,
     notes: notesReducer,
   },
+  middleware: [...getDefaultMiddleware(), routerMiddleware(history)],
 })
 
 export type RootState = ReturnType<typeof store.getState>
