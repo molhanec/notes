9df27d67c6fd91fbb96f9fd646748db17f273049
Michal Molhanec <michal@molhanec.net>
2020-04-02 20:19:07 +0200

    Add tests

--- src/routes/Main.test.tsx
+++ src/routes/Main.test.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
-import App from "./App"
+import { store } from "../app/store"
+import { MainRoute } from "./Main"
 
-test("<App> matches snapshot", () => {
+test("<Main> matches snapshot", () => {
   const { container } = render(
     <Provider store={store}>
-      <App />
+      <MainRoute />
     </Provider>
   )
 
